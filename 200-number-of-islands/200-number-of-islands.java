71c23d415edc5fef0329ff12f97ccff086a7553a
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-01-10 15:26:15 +0530

    Time: 9 ms (14.57%), Space: 55.8 MB (15.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,13 +1,3 @@
-class Pair{
-    int row;
-    int col;
-    
-    public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
     
     int dr[] = new int[]{0,0,1,-1};
@@ -24,58 +14,54 @@ class Solution {
         return false;
     }
     
-    private void islandbfs(char grid[][], int i, int j,boolean visited[][]){
+    private void dfsisland(char grid[][], int i, int j, boolean visited[][]){
         
-        Queue<Pair> q = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
         
-        q.add(new Pair(i,j));
-        // visited[i][j] = true;
-        
-        while(!q.isEmpty()){
-            
-            Pair front = q.poll();
-            // visited[front.row][front.col] = true;
-            
-                for(int k = 0; k < 4; k++){
-                int nr = front.row + dr[k];
-                int nc = front.col + dc[k];
-                    
-                   if(isValid(grid,nr,nc,visited)){
-                       q.add(new Pair(nr,nc));
-                       visited[nr][nc] = true;
-                   }    
-            }
-              
+        if(i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0'){
+            return;
         }
         
+        visited[i][j] = true;
         
+        for(int k = 0; k < 4; k++){
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+            
+            if(isValid(grid,nr,nc,visited)){
+                dfsisland(grid,nr,nc,visited);
+            }
+
+        }
+        
+    
         
         
     }
     
     public int numIslands(char[][] grid) {
         
-         if(grid==null || grid.length==0) return 0;
-        
         int m = grid.length;
         int n = grid[0].length;
         
         boolean visited[][] = new boolean[m][n];
         
-        
         int islands = 0;
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 
                 if(grid[i][j] == '1' && visited[i][j] == false){
                     visited[i][j] = true;
-                    islandbfs(grid,i,j,visited);
-                    islands++; 
+                    dfsisland(grid,i,j,visited);
+                    islands++;
+                    
                 }
                 
             }
         }
-        
         return islands;
+        
     }
 }
