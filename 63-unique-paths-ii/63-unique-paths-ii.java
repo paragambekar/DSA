0a216b92483ce73c624bb871b665735e9b53104c
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-01-26 07:00:15 +0530

    Time: 1 ms (45.47%), Space: 39.2 MB (24.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,45 +1,46 @@
 class Solution {
-    
-    private int upaths(int i, int j, int obstacleGrid[][], int dp[][]){
-            
-           
-            if((i == 0 && j == 0) && (obstacleGrid[i][j] == 0)){
-                return 1;
-            }
-            
-            if(i < 0 || j < 0){
-                return 0;
-            }
-        
-             if(obstacleGrid[i][j] == 1){
-                return 0;
-            }
-            
-            
-            if(dp[i][j] != -1){
-                return dp[i][j];
-            }
-            
-            int up = upaths(i-1,j,obstacleGrid,dp);
-            int left = upaths(i,j-1,obstacleGrid,dp);
-            
-            return dp[i][j] = up+left;
-            
-        }
-    
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         
         int dp[][] = new int[m][n];
+        
+        if(obstacleGrid[0][0] == 0){
+            dp[0][0] = 1;
+        }else{
+            dp[0][0] = 0;
+        }
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                dp[i][j] = -1;
+                
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else{
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i > 0){
+                        up = dp[i-1][j];
+                    }
+                    
+                    if(j > 0){
+                        left = dp[i][j-1];
+                    }
+                    
+                    dp[i][j] = left + up;
+                    
+                }
+                
             }
         }
         
-        return upaths(m-1,n-1,obstacleGrid,dp);
+        return dp[m-1][n-1];
         
     }
 }
