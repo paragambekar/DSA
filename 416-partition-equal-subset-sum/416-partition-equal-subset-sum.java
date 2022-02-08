0dc375f76d6736ef7c54b32be57b47ea277f8b1a
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-02-08 18:58:33 +0530

    Time: 165 ms (9.28%), Space: 70.6 MB (13.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,61 +1,47 @@
 class Solution {
     
-    private boolean partition(int nums[], int idx, int target,Boolean dp[][]){
+    public boolean subset(int nums[], int i, int target, Boolean dp[][]){
         
         if(target == 0){
             return true;
         }
         
-        if(idx == 0 && nums[idx] == target){
-            return true;
-        }
-        
-         if(idx == 0 && nums[idx] != target){
+        if(i < 0){
             return false;
         }
         
-        
-        if(dp[idx][target] != null){
-           return dp[idx][target];
+        if(dp[i][target] != null){
+            return dp[i][target];
         }
         
-        boolean notTake = partition(nums,idx-1,target,dp);
-        boolean take = false;
-        if(nums[idx] <= target){
-            take = partition(nums,idx-1,target-nums[idx],dp);  
+        boolean notPick = subset(nums,i-1,target,dp);
+        boolean pick = false;
+        if(nums[i] <=  target){
+            pick = subset(nums,i-1,target-nums[i],dp);
         }
         
-        return dp[idx][target] = take || notTake;
+        return dp[i][target] = notPick || pick;
+        
     }
     
     public boolean canPartition(int[] nums) {
         
-        int target = 0;
+        int sum = 0;
         for(int i = 0; i < nums.length; i++){
-            target += nums[i];
+            sum+=nums[i];
         }
-        // System.out.println(target);
         
-        if(target % 2 == 1){
+        if(sum % 2 == 1){
             return false;
         }
         
-        int m = nums.length;
-        // System.out.println(m);
+        int target = sum / 2;
         
-        int n = target / 2 ;
-        // System.out.println(n);
+        Boolean dp[][] = new Boolean[nums.length][target+1];
         
-        Boolean dp[][] = new Boolean[m][n+1];
+        return subset(nums, nums.length-1,target,dp);
         
-        // for(int i = 0; i < m; i++){
-        //     for(int j = 0; j <= n; j++){
-        //         dp[i][j] = -1;
-        //     }
-        // }
-       
         
-        return partition(nums,m-1,n,dp);
         
     }
 }
