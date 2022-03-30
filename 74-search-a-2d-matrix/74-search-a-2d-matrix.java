f5f0bd711a54675890181f86e302096df0da7f3b
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-03-30 09:33:08 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (93.38%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,38 +1,34 @@
 class Solution {
     
-    private boolean isValid(int matrix[][], int i, int j){
-        int m = matrix.length;
-        int n = matrix[0].length;
+    public boolean isValid(int grid[][], int i, int j){
         
-        if(i >= 0 && i < m && j >= 0 && j < n){
-            return true;
-        }
+        int n = grid.length;
+        int m = grid[0].length;
         
-        return false;
+        if(i < 0 || i >= n || j < 0 || j >= m) return false;
         
-    }
-    
-    private boolean search(int matrix[][], int i, int j, int target){
-        
-        while(true){
-        if(isValid(matrix,i,j)){
-            if(target == matrix[i][j]){
-                return true;
-            }else if(target < matrix[i][j]){
-                j--;
-            }else if(target > matrix[i][j]){
-                i++;
-            }
-        }else{
-              return false;  
-            }
-        }
+        return true;
         
     }
     
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        return search(matrix,0,matrix[0].length-1,target);
+        int i = matrix.length-1;
+        int j = 0;
+        
+        while(isValid(matrix,i,j)){
+            
+            if(matrix[i][j] == target) return true;
+            
+            if(matrix[i][j] > target){
+                i--;
+            }else{
+                j++;
+            }
+            
+        }
+        
+        return false;
         
     }
 }
