3711207d8ebf0ed07f46c7b8a09d6ed2d32c4245
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-04-01 08:41:54 +0530

    Time: 0 ms (100.00%), Space: 49.3 MB (84.59%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    public void swap(char[] s, int i, int j){
+        
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        
+    }
+    
+    public void reverseString(char[] s) {
+        
+        int left = 0;
+        int right = s.length-1;
+        
+        while(left < right){
+            
+            swap(s,left,right);
+            left++;
+            right--;
+            
+        }
+        
+    }
+}
