fb12904feb70aab96adb62301f2fce5b416c225b
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-10 13:18:26 +0530

    Time: 2 ms (93.50%), Space: 47.9 MB (25.81%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,43 +1,37 @@
 class Solution {
+    
+    public int fallingSum(int i, int j, int grid[][], int dp[][]){
+        
+        if(i < 0 || j < 0 || j >= grid.length) return Integer.MAX_VALUE;
+        
+        if(i == 0) return grid[i][j];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int up = fallingSum(i-1,j,grid,dp);
+        int left = fallingSum(i-1,j-1,grid,dp);
+        int right = fallingSum(i-1,j+1,grid,dp);
+        
+        return dp[i][j] = grid[i][j] + Math.min(up, Math.min(left,right));
+        
+    }
+    
     public int minFallingPathSum(int[][] matrix) {
         
+        int min = Integer.MAX_VALUE;
         int n = matrix.length;
         
         int dp[][] = new int[n][n];
-        
-        for(int i = n-1; i >=0; i--){
-            for(int j = n-1; j >=0; j--){
-                
-                // base case
-                if(i == n-1){
-                    dp[i][j] = matrix[i][j];
-                }else{
-                    
-                    int down = dp[i+1][j];
-                    int leftd = Integer.MAX_VALUE;
-                    int rightd = Integer.MAX_VALUE;
-                    
-                    if(j > 0){
-                        leftd = dp[i+1][j-1];
-                    }
-                    
-                    if(j+1 < n){
-                        rightd = dp[i+1][j+1];
-                    }
-                    
-                    dp[i][j] = matrix[i][j] +  Math.min(down,Math.min(leftd,rightd));
-                    
-                }
-                
-            }
+        for(int row[] : dp){
+            Arrays.fill(row, -1);
         }
         
-        int minsum = dp[0][0];
-        for(int i = 1; i < n; i++){
-            minsum = Math.min(minsum,dp[0][i]);
+        for(int i = 0; i < n; i++){
+            int ans = fallingSum(n-1, i, matrix,dp);
+            min = Math.min(ans,min);
         }
         
-        return minsum;
+        return min;
         
     }
 }
