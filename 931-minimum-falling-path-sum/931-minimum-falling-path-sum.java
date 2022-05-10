5d2c1824667323216172204d0dd70f00a1c5b893
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-10 13:25:20 +0530

    Time: 10 ms (15.86%), Space: 46.7 MB (70.26%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -22,16 +22,46 @@ class Solution {
         int n = matrix.length;
         
         int dp[][] = new int[n][n];
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
-        }
         
         for(int i = 0; i < n; i++){
-            int ans = fallingSum(n-1, i, matrix,dp);
-            min = Math.min(ans,min);
+            dp[0][i] = matrix[0][i];
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                
+                int up = Integer.MAX_VALUE;
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+                
+                if(i > 0) up = dp[i-1][j];
+                if(i > 0 && j > 0) left = dp[i-1][j-1];
+                if(i > 0 && j < n-1) right = dp[i-1][j+1];
+                
+                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(left,right));
+                
+            }
+        }
+        
+        // int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            min = Math.min(min, dp[n-1][i]);
+            
         }
         
         return min;
+        
+        // for(int row[] : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        
+        
+        // for(int i = 0; i < n; i++){
+        //     int ans = fallingSum(n-1, i, matrix,dp);
+        //     min = Math.min(ans,min);
+        // }
+        
+        // return min;
         
     }
 }
