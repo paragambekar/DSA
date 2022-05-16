1eb08212c54bdb03aaf83d155ded00e51e35ec99
Parag Ambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-16 23:20:30 +0530

    Time: 14 ms (75.74%), Space: 45.7 MB (91.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -19,11 +19,54 @@ class Solution {
         int l1 = text1.length();
         int l2 = text2.length();
         int dp[][] = new int[l1][l2];
-        for(int row[]: dp){
-            Arrays.fill(row,-1);
+        
+        
+        for(int idx1 = 0; idx1 < l1; idx1++){
+            for(int idx2 = 0; idx2 < l2; idx2++){
+               
+               // System.out.println("Inside loop");
+                if(text1.charAt(idx1) == text2.charAt(idx2)){
+                    // System.out.println("Does match");
+                    int prevAns = 0;
+                    if(idx1-1 >= 0 && idx2-1>= 0){
+                        prevAns = dp[idx1-1][idx2-1];
+                    }
+                    dp[idx1][idx2] =  1 + prevAns;
+                }else{
+                    int pos1 = 0;
+                if(idx1-1 >= 0 && idx2>= 0){
+                    pos1 = dp[idx1-1][idx2];
+                }
+                
+                int pos2 = 0;
+                if(idx1 >= 0 && idx2-1>= 0){
+                    pos2 = dp[idx1][idx2-1];
+                }
+                
+                dp[idx1][idx2] = Math.max(pos1,pos2);
+                }
+                
+                
+                
+                
+                
+            }
         }
         
-        return lcs(text1,text2,l1-1,l2-1,dp);
+        // for(int i  = 0; i < dp.length; i++){
+        //     for(int j = 0; j < dp[0].length; j++){
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        return dp[l1-1][l2-1];
+        
+//         for(int row[]: dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return lcs(text1,text2,l1-1,l2-1,dp);
         
     }
 }
