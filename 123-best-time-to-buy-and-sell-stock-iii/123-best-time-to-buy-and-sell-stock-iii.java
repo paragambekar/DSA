bc6db4814e31a79f8312e557ed2749f95f42800d
Parag Ambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-24 21:27:44 +0530

    Time: 117 ms (20.79%), Space: 124.1 MB (15.45%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -25,15 +25,50 @@ class Solution {
     
     public int maxProfit(int[] prices) {
         
-        int dp[][][] = new int[prices.length+1][2][3];
-        for(int i = 0; i <= prices.length; i++){
-            for(int j = 0; j < 2; j++){
-                for(int k = 0; k < 3; k++){
-                    dp[i][j][k] = -1;
+        int n = prices.length;
+        int dp[][][] = new int[n+1][2][3];
+        // for(int i = 0; i <= prices.length; i++){
+        //     for(int j = 0; j < 2; j++){
+        //         for(int k = 0; k < 3; k++){
+        //             dp[i][j][k] = -1;
+        //         }
+        //     }
+        // }
+        
+        for(int idx = 0; idx <= n; idx++){
+            for(int buy = 0; buy <=1; buy++){
+                dp[idx][buy][0] = 0;
+            }
+        }
+        
+        for(int buy = 0; buy <= 1; buy++){
+            for(int cap = 0; cap <=2; cap++){
+                dp[0][buy][cap] = 0;
+            }
+        }
+        
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 2; cap > 0; cap--){
+                    
+                    
+                    if(buy == 1){
+                          int canBuy = -prices[idx] + dp[idx+1][0][cap];
+                          int skipBuy = 0 + dp[idx+1][1][cap];
+
+                          dp[idx][buy][cap] = Math.max(canBuy,skipBuy);
+                    }else{
+                         int sell = prices[idx] + dp[idx+1][1][cap-1];
+                        int skipSell = 0 + dp[idx+1][0][cap];
+
+                         dp[idx][buy][cap] = Math.max(sell,skipSell);
+                    }
+                    
                 }
             }
         }
         
-        return profit(0,1,2,prices,dp);
+        return dp[0][1][2];
+        // return profit(0,1,2,prices,dp);
     }
 }
