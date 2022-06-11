c33400c93717404d4c5b43fee2283d5ed8c2bc5b
Parag Ambekar <69147632+paragambekar@users.noreply.github.com>
2022-06-11 11:31:26 +0530

    Time: 46 ms (9.19%), Space: 50.2 MB (48.63%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -27,12 +27,34 @@ class Solution {
     
     public int numDistinct(String s, String t) {
         
-        int dp[][] = new int[s.length()][t.length()];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[s.length()+1][t.length()+1];
+//         for(int row[] : dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return countWays(s,t,s.length()-1, t.length()-1,dp);
+        
+        for(int i = 0; i <= s.length(); i++){
+            dp[i][0] = 1;
         }
         
-        return countWays(s,t,s.length()-1, t.length()-1,dp);
+        for(int i = 1; i <= t.length(); i++) dp[0][i] = 0;
+        
+        for(int idx1 = 1; idx1 <= s.length(); idx1++){
+            for(int idx2 = 1; idx2 <= t.length(); idx2++){
+                
+                
+                if(s.charAt(idx1-1) == t.charAt(idx2-1)){
+                     dp[idx1][idx2] =  dp[idx1-1][idx2-1] + dp[idx1-1][idx2]; 
+                }else{
+                     dp[idx1][idx2] =  dp[idx1-1][idx2];
+                }
+                
+            }
+        }
+        
+        return dp[s.length()][t.length()];
+        
         
     }
 }
