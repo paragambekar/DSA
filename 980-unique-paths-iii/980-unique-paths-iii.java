3e441357fd44ba36413e9b45254428d3b2a1292b
Parag Ambekar <69147632+paragambekar@users.noreply.github.com>
2022-06-15 17:31:26 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (97.66%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.java
+++ 980-unique-paths-iii/980-unique-paths-iii.java
@@ -1,41 +1,28 @@
 class Solution {
     
-    public int paths(int grid[][],int x, int y, int destR, int destC, int obstacles, int count, boolean visited[][]){
+    public int paths(int grid[][],int x, int y,int obstacles, int count, boolean visited[][]){
         
         int u = grid.length;
         int v = grid[0].length;
         
-        
         if(x < 0 || y < 0 || x >= u || y >= v || grid[x][y] == -1 || (visited[x][y] == true)) return 0;
-        System.out.println("count" + count);
-       // visited[x][y] = true; 
+       
         
         if(grid[x][y] == 2){
-            
             if(count == ((u*v)-obstacles-1)){
-                for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                
-                System.out.print(visited[i][j] + " ");
-            }
-            System.out.println();
-        }
-          System.out.println("count" + count);
-                System.out.println();
                 return 1;
             }else{
                 return 0;
             }
-            
         }
         
         
         visited[x][y] = true;
         
-        int up = paths(grid,x-1,y,destR,destC,obstacles,count+1,visited);
-        int right = paths(grid,x,y+1,destR,destC,obstacles,count+1,visited);
-        int down = paths(grid,x+1,y,destR,destC,obstacles,count+1,visited);
-        int left = paths(grid,x,y-1,destR,destC,obstacles,count+1,visited);
+        int up = paths(grid,x-1,y,obstacles,count+1,visited);
+        int right = paths(grid,x,y+1,obstacles,count+1,visited);
+        int down = paths(grid,x+1,y,obstacles,count+1,visited);
+        int left = paths(grid,x,y-1,obstacles,count+1,visited);
         
         visited[x][y] = false;
         
@@ -68,13 +55,7 @@ class Solution {
             }
         }
         
-        System.out.println(startR);
-        System.out.println(startC);
-        System.out.println(destR);
-        System.out.println(destC);
-        System.out.println(obstacles);
-        
-        return paths(grid,startR,startC,destR,destC,obstacles,0,visited);
+        return paths(grid,startR,startC,obstacles,0,visited);
         
         
     }
