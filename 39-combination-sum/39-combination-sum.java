85470fa90b8764142632cbec0ab898e34690fb4b
Parag Ambekar <69147632+paragambekar@users.noreply.github.com>
2022-06-26 09:01:34 +0530

    Time: 3 ms (94.49%), Space: 45 MB (52.10%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -3,20 +3,32 @@ class Solution {
     
     public void helper(int nums[], int idx, int target, List<List<Integer>> ans, List<Integer> ds){
         
-        if(idx == nums.length){
+        // if(idx == nums.length){
             if(target == 0){
                 ans.add(new ArrayList<>(ds));
+                return;
             }
-            return;
+            
+        // }
+        
+        for(int i = idx; i < nums.length; i++){
+            if(nums[i] <= target){
+                ds.add(nums[i]);
+                helper(nums,i,target-nums[i],ans,ds);
+                ds.remove(ds.size()-1);
+            }
+            
+            // helper(nums,i+1,target,ans,ds);
+            
         }
         
-        if(target >= nums[idx]){
-        ds.add(nums[idx]);
-        helper(nums,idx,target-nums[idx],ans,ds);
-        ds.remove(ds.size()-1);
-        }
+//         if(target >= nums[idx]){
+//         ds.add(nums[idx]);
+//         helper(nums,idx,target-nums[idx],ans,ds);
+//         ds.remove(ds.size()-1);
+//         }
         
-        helper(nums,idx+1,target,ans,ds);
+//         helper(nums,idx+1,target,ans,ds);
         
     }
     
